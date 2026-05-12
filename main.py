0d24fe0936289c9ec74e45e6e3a56311480f1fad
letstrystuff <niceforwork23@gmail.com>
2026-05-12 11:53:25 +0700

    Repo Aplikasi Penerbangan

---
+++ main.py
@@ -0,0 +1,26 @@
+class Pesawat:
+    def __init__(self, **kwargs):
+        self.nama = kwargs.get("nama")
+        self.kapasitas = kwargs.get("kapasitas")
+        self.kecepatan = kwargs.get("kecepatan")
+        self.kemampuan_jelajah = kwargs.get("kemampuan jelajah")
+        self.id_penerbangan = kwargs.get("ID")
+
+class Tiket:
+    def __init__(self, **kwargs):
+        pass
+
+class Pelanggan:
+    def __init__(self, **kwargs):
+        pass
+
+class Penerbangan:
+    def __init__(self, **kwargs):
+        pass
+
+
+def main():
+    exit()
+
+if __name__=="main":
+    main()
